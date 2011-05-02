599afe476004e7dc5a86e29bc5fe8fbc85f0a8dc
Simon Pratt <spratt@scs.carleton.ca>
2011-05-02 14:10:55 -0400

    Seems to work, not yet finding median

--- PrioritySearchTree.java
+++ PrioritySearchTree.java
@@ -21,25 +21,46 @@
 ******************************************************************************/
 
 import java.awt.geom.*;
+import java.util.*;
 
 public class PrioritySearchTree {
-    PSTNode root;
+    private PSTNode root;
 
+    // Assumes all points are valid (e.g. not null)
     public PrioritySearchTree(Point2D.Double[] points) {
 	// Find point with highest Y value
 	if(points == null || points.length < 1) return;
-	Point2D.Double root = points[0];
+	Point2D.Double rootPoint = points[0];
 	for(Point2D.Double p : points) {
-	    if(p.getY() > root.getY())
-		root = p;
+	    if(p.getY() > rootPoint.getY())
+		rootPoint = p;
 	}
 	// Find median X value
-	// Make lower point array
-	// Make upper point array
+	double medianX = 0.0d;
+	// Make upper and lower point array
+	List<Point2D.Double> upperPoints = new ArrayList<Point2D.Double>();
+	List<Point2D.Double> lowerPoints = new ArrayList<Point2D.Double>();
+	for(Point2D.Double p : points) {
+	    if(p == rootPoint) continue;
+	    else if(p.getX() < medianX) lowerPoints.add(p);
+	    else upperPoints.add(p);
+	}
 	// Make tree
+	this.root = new PSTNode(rootPoint,medianX);
+	this.root.setLeftChild((new PrioritySearchTree(lowerPoints.toArray(new Point2D.Double[0]))).getRoot());
+	this.root.setRightChild((new PrioritySearchTree(upperPoints.toArray(new Point2D.Double[0]))).getRoot());
     }
+
+    public PSTNode getRoot() { return root; }
 
     public static void main(String[] args) {
 	new PrioritySearchTree(null);
+	Point2D.Double[] testPoints = new Point2D.Double[5];
+	testPoints[0] = new Point2D.Double(2.0d,5.0d);
+	testPoints[1] = new Point2D.Double(-2.0d,4.0d);
+	testPoints[2] = new Point2D.Double(1.0d,3.0d);
+	testPoints[3] = new Point2D.Double(-1.0d,2.0d);
+	testPoints[4] = new Point2D.Double(0.0d,1.0d);
+	new PrioritySearchTree(testPoints);
     }
 }
